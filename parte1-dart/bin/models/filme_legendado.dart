dab502a877e2c6d5be27221c40c38f7484e744a3
Arthur <brolezearthur@gmail.com>
2026-09-13 10:41:31 -0300

    Update filme_legendado.dart

--- parte1-dart/bin/models/filme_legendado.dart
+++ parte1-dart/bin/models/filme_legendado.dart
@@ -1,2 +1,29 @@
-// TODO: Exercício 2 — herança.
-// Recurso obrigatório: extends — Conceito: super no construtor + @override
+import 'filme.dart';
+
+/// Exercício 2 — Herança
+/// Recurso obrigatório: extends — Conceito: super no construtor + @override
+///
+/// Teste "é-um": todo filme legendado é um filme. Cabe herança.
+class FilmeLegendado extends Filme {
+  final String idioma;
+  final String estudioDublagem;
+
+  FilmeLegendado({
+    required String titulo,
+    required int duracaoMinutos,
+    required double preco,
+    DateTime? dataLancamento,
+    required this.idioma,
+    required this.estudioDublagem,
+  }) : super(
+          titulo: titulo,
+          duracaoMinutos: duracaoMinutos,
+          preco: preco,
+          dataLancamento: dataLancamento,
+        );
+
+  @override
+  String ficha() {
+    return '${super.ficha()}, $idioma, dublagem: $estudioDublagem';
+  }
+}
